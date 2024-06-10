19de8ffc984b76d5a7d5bf2ebebf3a1b0a71aa86
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-06-10 10:22:10 -0300

    task: criada a model e repository de categoria

---
+++ src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.farmacia.farmacia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farmacia.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
